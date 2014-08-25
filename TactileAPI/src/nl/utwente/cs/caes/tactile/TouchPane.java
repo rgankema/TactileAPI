fa40ddbe4680ba8df492c4acda40cb6b899c254b
Frumbasjt <richardgankema@gmail.com>
2014-08-25 13:17:47 +0200

    TouchPane aangemaakt

---
+++ TactileAPI/src/nl/utwente/cs/caes/tactile/TouchPane.java
@@ -0,0 +1,25 @@
+package nl.utwente.cs.caes.tactile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+public class TouchPane extends Pane {
+	private Map<Rectangle, InteractableGroup> objectByBounds = new HashMap<Rectangle, InteractableGroup>();
+	private QuadTree quadTree;
+	
+	public TouchPane() {
+		super();
+	}
+	
+	public TouchPane(Node... children){
+		super(children);
+	}
+
+	public void register(InteractableGroup object) {
+		
+	}
+}
