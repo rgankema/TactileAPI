157397ad22597db9365255f9dd35c36b3ebbfbea
Frumbasjt <richardgankema@gmail.com>
2014-08-25 13:41:55 +0200

    Updated TouchPane
    Heb een resize listener toegevoegd die de QuadTree update als de
    TouchPane van grootte veranderd. Extreem inefficient op het moment!

--- TactileAPI/src/nl/utwente/cs/caes/tactile/TouchPane.java
+++ TactileAPI/src/nl/utwente/cs/caes/tactile/TouchPane.java
@@ -5,12 +5,12 @@ import java.util.Map;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
 
 public class TouchPane extends Pane {
-	private Map<Rectangle, InteractableGroup> objectByBounds = new HashMap<Rectangle, InteractableGroup>();
+	private Map<Bounds, InteractableGroup> objectByBounds = new HashMap<Bounds, InteractableGroup>();
 	private QuadTree quadTree;
 	
 	public TouchPane() {
@@ -23,16 +23,30 @@ public class TouchPane extends Pane {
 	
 	private void addResizeListeners(){
 		TouchPane thisPane = this;
+		
+		// Can be optimised
 		widthProperty().addListener(new ChangeListener<Number>() {
 
 			@Override
 			public void changed(ObservableValue<? extends Number> observableValue,
 					Number oldWidth, Number newWidth) {
 				quadTree = new QuadTree(thisPane.localToScene(getBoundsInLocal()));
+				for (Bounds bounds : objectByBounds.keySet()) {
+					quadTree.insert(bounds);
+				}
 			}
-			
-			
-			
+		});
+		
+		heightProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observableValue,
+					Number oldHeight, Number newHeight) {
+				quadTree = new QuadTree(thisPane.localToScene(getBoundsInLocal()));
+				for (Bounds bounds : objectByBounds.keySet()) {
+					quadTree.insert(bounds);
+				}
+			}
 		});
 	}
 
