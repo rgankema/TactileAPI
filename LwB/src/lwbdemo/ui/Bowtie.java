06d926a09ec16fc43e28c2bf512cb36c1ab553b1
Frumbasjt <richardgankema@gmail.com>
2014-10-27 14:11:44 +0100

    Aanpassingen bowtie

--- LwB/src/lwbdemo/ui/Bowtie.java
+++ LwB/src/lwbdemo/ui/Bowtie.java
@@ -9,10 +9,10 @@ import javafx.animation.AnimationTimer;
 import javafx.geometry.Bounds;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Font;
 
@@ -24,36 +24,41 @@ public class Bowtie extends Group {
     private static final double INSET = 10;
     
     final Knot knot;
-    final Circle dummy;
     
     final HBox hbox;
+    final HBox leftBlade;
+    final HBox rightBlade;
     final Polygon background;
     
     public Bowtie(String term, String... type) {
         hbox = new HBox();
-        
+        leftBlade = new HBox();
+        rightBlade = new HBox();
         knot = new Knot(this);
-        dummy = new Circle(knot.getRadius());
-        dummy.setVisible(false);
         
-        knot.layoutXProperty().bind(dummy.layoutXProperty());
-        knot.layoutYProperty().bind(dummy.layoutYProperty());
+        leftBlade.getChildren().add(buildLabel(term));
+        leftBlade.setSpacing(2);
+        leftBlade.setAlignment(Pos.CENTER);
         
-        hbox.getChildren().add(buildLabel(term));
-        hbox.getChildren().add(dummy);
         for (String argument : type) {
-            hbox.getChildren().add(buildLabel(argument));
+            rightBlade.getChildren().add(buildLabel(argument));
+            rightBlade.getChildren().add(buildLabel("->"));
         }
-        hbox.getChildren().add(knot);
+        rightBlade.getChildren().remove(rightBlade.getChildren().size() - 1);
+        rightBlade.setSpacing(2);
+        rightBlade.setAlignment(Pos.CENTER);
+        
+        hbox.getChildren().addAll(leftBlade, knot, rightBlade);
         hbox.setSpacing(10);
         hbox.setAlignment(Pos.CENTER);
         
+        // The bowtie without knot
         background = new Polygon();
         background.setFill(Color.BISQUE);
         background.setStroke(Color.BROWN);
         
+        // Draws the bowtie
         new AnimationTimer() {
-
             @Override
             public void handle(long now) {
                 Bounds hboxBounds = hbox.getBoundsInParent();
@@ -61,24 +66,56 @@ public class Bowtie extends Group {
 
                 background.getPoints().clear();
                 background.getPoints().addAll(new Double[]{
+                    // Top left corner
                     hboxBounds.getMinX() - INSET, hboxBounds.getMinY() - INSET,
                     knotBounds.getMinX(), hboxBounds.getMinY() - INSET,
                     knotBounds.getMinX() + knotBounds.getWidth() / 2, hboxBounds.getMinY() + hboxBounds.getHeight() / 2,
                     knotBounds.getMaxX(), hboxBounds.getMinY() - INSET,
+                    // Top right corner
                     hboxBounds.getMaxX() + INSET, hboxBounds.getMinY() - INSET,
-
+                    // Bottom right corner
                     hboxBounds.getMaxX() + INSET, hboxBounds.getMaxY() + INSET,
                     knotBounds.getMaxX(), hboxBounds.getMaxY() + INSET,
                     knotBounds.getMinX() + knotBounds.getWidth() / 2, hboxBounds.getMinY() + hboxBounds.getHeight() / 2,
                     knotBounds.getMinX(), hboxBounds.getMaxY() + INSET,
+                    // Bottom left corner
                     hboxBounds.getMinX() - INSET, hboxBounds.getMaxY() + INSET
                 });
             }
             
         }.start();
         
-        getChildren().add(background);
-        getChildren().add(hbox);
+        getChildren().addAll(background, hbox);
+    }
+    
+    public void exposeHole() {
+        if (!rightBlade.getChildren().isEmpty()) {
+            Node removeNode = rightBlade.getChildren().get(0);
+            rightBlade.getChildren().remove(removeNode);
+            if (!rightBlade.getChildren().isEmpty()) {
+                // remove arrow
+                rightBlade.getChildren().remove(0);
+            }
+            if (leftBlade.getChildren().size() > 1) {
+                leftBlade.getChildren().add(buildLabel("->"));
+            }
+            leftBlade.getChildren().add(removeNode);
+        }
+    }
+    
+    public void coverHole() {
+        if (leftBlade.getChildren().size() > 1) {
+            Node removeNode = leftBlade.getChildren().get(leftBlade.getChildren().size() - 1);
+            leftBlade.getChildren().remove(removeNode);
+            if (!rightBlade.getChildren().isEmpty()) {
+                // add arrow
+                rightBlade.getChildren().add(0, buildLabel("->"));
+            }
+            if (leftBlade.getChildren().size() > 2) {
+                leftBlade.getChildren().remove(leftBlade.getChildren().size() - 1);
+            }
+            rightBlade.getChildren().add(0, removeNode);
+        }
     }
     
     private Label buildLabel(String text) {
--- LwB/src/lwbdemo/ui/Knot.java
+++ LwB/src/lwbdemo/ui/Knot.java
@@ -6,44 +6,33 @@
 package lwbdemo.ui;
 
 import javafx.geometry.Bounds;
-import javafx.geometry.Point2D;
 import javafx.scene.Node;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Knot extends Circle {
     private static final double RADIUS = 15;
     
-    final HBox parent;
-    final Bowtie bowtie;
-    
-    double anchorX;
+    private double anchorX;
     
     protected Knot(Bowtie bowtie) {
         super(RADIUS);
         
-        setManaged(false);
-        this.bowtie = bowtie;
-        this.parent = bowtie.hbox;
-        
         setOnMousePressed(event -> {
+            // Record location of click event in Knot
             anchorX = event.getX();
             event.consume();
         });
         
         setOnMouseDragged(event -> {
-            int position = parent.getChildren().indexOf(bowtie.dummy);
-            
-            Bounds thisBounds = getBoundsInParent();
+            Bounds thisBounds = this.localToScene(getBoundsInLocal());
             double thisCenterX = thisBounds.getMinX() + thisBounds.getWidth() / 2;
             
-            Point2D pointInParent = parent.sceneToLocal(new Point2D(event.getSceneX(), event.getSceneY()));
-            double x = pointInParent.getX() - anchorX;
+            double x = event.getSceneX() - anchorX;
             
-            if (x < thisCenterX && position > 1) {
-                Node left = parent.getChildren().get(position - 1);
-                Bounds leftBounds = left.getBoundsInParent();
+            if (x < thisCenterX && bowtie.leftBlade.getChildren().size() > 1) {
+                Node left = bowtie.leftBlade.getChildren().get(bowtie.leftBlade.getChildren().size() - 1);
+                Bounds leftBounds = left.localToScene(left.getBoundsInLocal());
                 double leftCenterX = leftBounds.getMinX() + leftBounds.getWidth() / 2;
                 
                 double delta = thisCenterX - leftCenterX;
@@ -56,15 +45,13 @@ public class Knot extends Circle {
                 setTranslateX(-RADIUS * progress);
                 
                 if (x < leftCenterX) {
-                    parent.getChildren().remove(bowtie.dummy);
-                    position--;
-                    parent.getChildren().add(position, bowtie.dummy);
+                    bowtie.coverHole();
                 }
             }
             
-            if (x > thisCenterX && position < parent.getChildren().size() - 2) {
-                Node right = parent.getChildren().get(position + 1);
-                Bounds rightBounds = right.getBoundsInParent();
+            if (x > thisCenterX && bowtie.rightBlade.getChildren().size() > 0) {
+                Node right = bowtie.rightBlade.getChildren().get(0);
+                Bounds rightBounds = right.localToScene(right.getBoundsInLocal());
                 double rightCenterX = rightBounds.getMinX() + rightBounds.getWidth() / 2;
                 
                 double delta = rightCenterX - thisCenterX;
@@ -76,12 +63,11 @@ public class Knot extends Circle {
                 setScaleY(1 - progress * 0.3);
                 setTranslateX(RADIUS * progress);
                 
-                if (pointInParent.getX() - anchorX > rightCenterX) {
-                    parent.getChildren().remove(bowtie.dummy);
-                    position++;
-                    parent.getChildren().add(position, bowtie.dummy);
+                if (x > rightCenterX) {
+                    bowtie.exposeHole();
                 }
             }
+            
             event.consume();
         });
         
